3149cf8b865550286f55908e702282257a41b7b9
BangSann <yosanokta12@gmail.com>
2024-09-25 19:36:14 +0700

    feat: sasasa

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: false })); // For parsing application/x-www-form-urlencoded
 // app.use(cors());
 
-app.use(express.static(path.resolve("./public")))
+// app.use(express.static(path.resolve("./public")))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router)
 
