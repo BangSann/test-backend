3fe58f58f1905dacdddb1b4341e08b8c0960be4b
BangSann <yosanokta12@gmail.com>
2024-09-25 15:38:14 +0700

    feat: sasasasas

--- api/api.js
+++ api/api.js
@@ -1,13 +1,20 @@
 import express from "express";
 // import Dongeng from "./Models/DongengModel.js";
-import { getDongeng , getDongengById } from "../controller/DongengController.js";
+import { countAllView, countDongeng, getDongeng , getDongengById, popularView, sumView } from "../controller/DongengController.js";
 import { register, verify } from "../controller/AuthController.js";
 
 const router = express.Router();
 
+//dongeng
 router.get("/api/dongeng/:id", getDongengById);
-router.post("/api/register", register);
 router.get("/api/dongeng", getDongeng);
+router.get("/api/count/dongeng" , countDongeng)
+router.get("/api/count/view" , countAllView)
+router.get("/api/popular" , popularView)
+router.get("/api/dongengview/:id" , sumView)
+//dongeng - end
+
+router.post("/api/register", register);
 router.get("/api/verify", verify);
 
 export default router;
--- controller/DongengController.js
+++ controller/DongengController.js
@@ -21,3 +21,52 @@ export const getDongengById = async (req, res) => {
     return res.status(401).json({ message: err.message });
   }
 };
+
+export const countDongeng = async (req, res) => {
+  try {
+    const response = (await Dongeng.findAll()).length;
+    return res.status(200).json({ row: response });
+  } catch (err) {
+    return res.status(401).json({ message: err.message });
+  }
+};
+
+export const countAllView = async (req, res) => {
+  try {
+    const views = await Dongeng.findAll({ attributes: ["view"] });
+    let viewAll = 0;
+    views.map((view) => {
+      // console.log(view.view)
+      viewAll += view.view;
+    });
+    return res.status(200).json({ views: viewAll });
+  } catch (err) {
+    return res.status(401).json({ message: err.message });
+  }
+};
+
+export const popularView = async (req, res) => {
+  try {
+    const result = await Dongeng.findAll({
+      limit: 4,
+      order: [["view", "DESC"]],
+    });
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const sumView = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const raw = await Dongeng.findOne({
+      where: { id },
+    });
+
+    raw.update({ view: raw.view + 1 });
+    return res.status(200).json({ message: "Berhasil menambah view" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
