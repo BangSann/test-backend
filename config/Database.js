d2999c1edea225a02f1302fe7adb42fdd7ed662d
BangSann <yosanokta12@gmail.com>
2024-09-25 19:15:51 +0700

    feat: sashasha

--- config/Database.js
+++ config/Database.js
@@ -1,20 +1,19 @@
 import { Sequelize } from "sequelize";
 import mysql from "mysql2";
 
-const db = new Sequelize("b2ysrs0ge9xzwhcyr5ic", "uan8q6bqn2zfixz7", "LcqQeMGhNTiHiBEBTU4k", {
-    host: "b2ysrs0ge9xzwhcyr5ic-mysql.services.clever-cloud.com",
-    dialect: "mysql",
-    dialectModule: mysql,
+const db = new Sequelize("u216976409_dongeng", "u216976409_CVSrikandiKr", "e2J0sR+Q",{
+//   password: "@W34@h9Z",
+  host: "154.41.240.52",
+  dialect: "mysql",
+  dialectModule: mysql,
 });
 
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-db
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
-export default db
+export default db;
