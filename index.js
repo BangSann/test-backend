6ad098893a4f728f598465edef8952f2b7a00b22
BangSann <yosanokta12@gmail.com>
2024-09-26 21:19:23 +0700

    fix: file

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use(cors());
 
 app.use(express.static(path.resolve("./public")))
 // app.use(express.static(path.join('public')));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 app.use(router)
 
