784c5f981e62ebe012a34677f766df966a12fc31
alungggggg <aldinoalungputraanugraha@gmail.com>
2024-09-30 10:28:36 +0700

    reset

--- index.js
+++ index.js
@@ -9,40 +9,35 @@ import { fileURLToPath } from "url";
 const app = express();
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: false })); // For parsing application/x-www-form-urlencoded
-// app.use(cors({
-//   origin : "https://ceritapanjikediri.my.id"
-// }));
+app.use(cors({
+  origin : "https://ceritapanjikediri.my.id"
+}));
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   if (req.url.endsWith(".js")) {
-//     res.setHeader("Content-Type", "application/javascript");
-//   }
-//   next();
-// });
-// app.use((req, res, next) => {
-//   res.setHeader("Content-Type", "application/javascript");
-//   next();
-// });
+app.use((req, res, next) => {
+  if (req.url.endsWith(".js")) {
+    res.setHeader("Content-Type", "application/javascript");
+  }
+  next();
+});
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/javascript");
+  next();
+});
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
-app.use(cors({
-  origin: 'https://ceritapanjikediri.my.id', // Izinkan origin ini
-  credentials: true, // Izinkan kredensial jika diperlukan (untuk cookies atau header otentikasi)
-}));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(router);
 
