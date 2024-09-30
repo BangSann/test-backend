2e401343a5d6f940a1002f8dc8d108fcc5f31cf6
alungggggg <aldinoalungputraanugraha@gmail.com>
2024-09-30 10:14:15 +0700

    test

--- index.js
+++ index.js
@@ -9,34 +9,52 @@ import { fileURLToPath } from "url";
 const app = express();
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: false })); // For parsing application/x-www-form-urlencoded
+// app.use(cors({
+//   origin : "https://ceritapanjikediri.my.id"
+// }));
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use((req, res, next) => {
+//   if (req.url.endsWith(".js")) {
+//     res.setHeader("Content-Type", "application/javascript");
+//   }
+//   next();
+// });
+// app.use((req, res, next) => {
+//   res.setHeader("Content-Type", "application/javascript");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+const allowedOrigins = ['http://localhost:5173', 'https://ceritapanjikediri.my.id'];
+
 app.use(cors({
-  origin : "https://ceritapanjikediri.my.id"
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow cookies or authentication headers
 }));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use((req, res, next) => {
-  if (req.url.endsWith(".js")) {
-    res.setHeader("Content-Type", "application/javascript");
-  }
-  next();
-});
-app.use((req, res, next) => {
-  res.setHeader("Content-Type", "application/javascript");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+app.get('/pdf/:fileName', (req, res) => {
+  // Serve your file, e.g., KEONG MAS.pdf
+  res.sendFile(`path/to/your/pdf/${req.params.fileName}`);
 });
 
 app.use(router);
