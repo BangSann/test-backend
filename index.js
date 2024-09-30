3b03d00ae83b18e2daf3f61dd6bb7f4e00c61c86
Dani Ardiansyach <ardiansyachd5@gmail.com>
2024-09-30 11:29:07 +0700

    fix:origin

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
