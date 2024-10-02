cac93f9d865702a6c4e8a95fba4bea5978f8d53d
BangSann <yosanokta12@gmail.com>
2024-10-02 16:40:10 +0700

    fix: dongeng model

--- Models/DongengModel.js
+++ Models/DongengModel.js
@@ -3,24 +3,6 @@ import db from "../config/Database.js";
 
 const { DataTypes } = Sequelize;
 
-const Dongeng = db.define(
-  "dongeng",
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: DataTypes.STRING,
-    fileName: DataTypes.STRING,
-    PdfPath: DataTypes.STRING,
-    view: DataTypes.INTEGER,
-    cover: DataTypes.STRING,
-  },
-  { freezeTableName: true }
-);
-
-// title:varchar, pdfURL:varchar, cover:varchar, filename:varchar
 // const Dongeng = db.define(
 //   "dongeng",
 //   {
@@ -30,12 +12,30 @@ const Dongeng = db.define(
 //       autoIncrement: true,
 //     },
 //     title: DataTypes.STRING,
-//     pdfURL: DataTypes.STRING,
-//     cover: DataTypes.STRING,
 //     fileName: DataTypes.STRING,
+//     PdfPath: DataTypes.STRING,
 //     view: DataTypes.INTEGER,
+//     cover: DataTypes.STRING,
 //   },
 //   { freezeTableName: true }
 // );
+
+// title:varchar, pdfURL:varchar, cover:varchar, filename:varchar
+const Dongeng = db.define(
+  "dongeng",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    pdfURL: DataTypes.STRING,
+    cover: DataTypes.STRING,
+    fileName: DataTypes.STRING,
+    view: DataTypes.INTEGER,
+  },
+  { freezeTableName: true }
+);
 
 export default Dongeng;
