1bc129f6d1606d54fa774d48e8d0da1e432a28bb
Dani Ardiansyach <ardiansyachd5@gmail.com>
2024-10-03 10:06:40 +0700

    bba

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(fileupload({
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "uploadedImg")));
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
